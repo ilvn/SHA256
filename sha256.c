5ee3817ef233d562a385731344a566acff2c151f
Ilya Levin <eli@literatecode.com>
2017-04-14 12:16:49 +0800

    fix inconsistent ctx

--- sha256.c
+++ sha256.c
@@ -129,7 +129,7 @@ static void _hash(sha256_context *ctx)
 } /* _hash */
 
 /* -------------------------------------------------------------------------- */
-void sha256_init(sha256_context ctx[1])
+void sha256_init(sha256_context *ctx)
 {
     ctx->len[0] = ctx->len[1] = 0;
     ctx->hash[0] = 0x6a09e667; ctx->hash[1] = 0xbb67ae85;
